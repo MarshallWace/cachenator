e998836f14c0240516cdc80370026a34737c913d
Adrian Chifor <a.chifor@mwam.com>
2021-04-01 16:49:50 +0100

    Update go.mod and test localstack

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -12,9 +12,9 @@ source $DIR/helpers.sh
 trap cleanup ERR
 
 echo -e "\nRunning AWS S3 localstack"
-docker run -d --name localstack-s3 -e SERVICES=s3 -p 4566:4566 localstack/localstack:0.12.2
-echo -e "Waiting 30s for AWS S3 localstack to be ready ..."
-sleep 30
+docker run -d --name localstack-s3 -e SERVICES=s3 -p 4566:4566 localstack/localstack:0.12.9
+echo -e "Waiting 45s for AWS S3 localstack to be ready ..."
+sleep 45
 
 echo -e "Creating test S3 bucket"
 aws --endpoint=$AWS_ENDPOINT s3api create-bucket --bucket $BUCKET
