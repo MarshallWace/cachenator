5eb50a7ee4c8b494fe16a1ff1027f0599b74e370
Tom Powell <T.Powell@mwam.com>
2022-01-17 11:18:35 +0000

    Parameter validation for both s3transparent and rest for key, prefix and bucket
    Signed-off-by: Tom Powell <T.Powell@mwam.com>

--- jwt.go
+++ jwt.go
@@ -26,6 +26,16 @@ type AuthHeader struct {
 	Authorization string `header:"Authorization"`
 }
 
+func getRequestParam(c *gin.Context, paramKey string) string {
+	if c.Param(paramKey) != "" {
+		return c.Param(paramKey)
+	} else if c.Query(paramKey) != "" {
+		return c.Query(paramKey)
+	} else {
+		return ""
+	}
+}
+
 func jwtMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		if strings.HasPrefix(c.Request.RequestURI, "/healthz") {
@@ -102,22 +112,32 @@ func jwtMiddleware() gin.HandlerFunc {
 				return
 			}
 
-			// TODO: Extract key/bucket from c.Param as well depending on type of request
-			// TODO: Validate key for methods where it makes sense, not all
-			if !strings.HasPrefix(c.Query("key"), strings.TrimSpace(claims.Prefix)) {
-				log.Debugf("JWT token prefix does not match URL object (prefix %s != object %s)", claims.Prefix, strings.TrimSpace(c.Query("key")))
-				jwtRequestsMetric.WithLabelValues("false", "JWT token prefix does not match URL object").Inc()
-				c.JSON(401, gin.H{"error": "JWT token prefix does not match URL object"})
-				c.Abort()
-				return
+			keyParam := getRequestParam(c, "key")
+			bucketParam := getRequestParam(c, "bucket")
+
+			if keyParam == "" {
+				keyParam = getRequestParam(c, "prefix")
 			}
 
-			if strings.TrimSpace(claims.Bucket) != "" && strings.TrimSpace(claims.Bucket) != c.Query("bucket") {
-				log.Debugf("JWT token bucket does not match URL bucket (jwt bucket %s != URL bucket %s", claims.Bucket, strings.TrimSpace(c.Query("bucket")))
-				jwtRequestsMetric.WithLabelValues("false", "JWT token bucket does not match URL bucket").Inc()
-				c.JSON(401, gin.H{"error": "JWT token bucket does not match URL bucket"})
-				c.Abort()
-				return
+			// Validate JWT bucket and key for all calls except ListBuckets (GET on "/" without bucket or key defined)
+			if (c.Request.Method != "GET" && c.Request.Method != "HEAD") || bucketParam != "" || keyParam != "" {
+
+				// Compare request bucket and key params with JWT bucket and key params
+				if !strings.HasPrefix(keyParam, strings.TrimSpace(claims.Prefix)) {
+					log.Debugf("JWT token prefix does not match URL object (prefix %s != object %s)", claims.Prefix, strings.TrimSpace(keyParam))
+					jwtRequestsMetric.WithLabelValues("false", "JWT token prefix does not match URL object").Inc()
+					c.JSON(401, gin.H{"error": "JWT token prefix does not match URL object"})
+					c.Abort()
+					return
+				}
+
+				if strings.TrimSpace(claims.Bucket) != "" && strings.TrimSpace(claims.Bucket) != bucketParam {
+					log.Debugf("JWT token bucket does not match URL bucket (jwt bucket %s != URL bucket %s", claims.Bucket, strings.TrimSpace(bucketParam))
+					jwtRequestsMetric.WithLabelValues("false", "JWT token bucket does not match URL bucket").Inc()
+					c.JSON(401, gin.H{"error": "JWT token bucket does not match URL bucket"})
+					c.Abort()
+					return
+				}
 			}
 
 			log.Debugf("Got valid JWT token, exiting JWT middleware")
